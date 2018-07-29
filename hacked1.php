0f9608f96bad890cb8c8060ce3a2c915a676c6ed
riskis7 <41503041+riskis7@users.noreply.github.com>
2018-07-29 15:45:29 +0700

    Add files via upload

---
+++ hacked1.php
@@ -0,0 +1,30 @@
+<?php
+error_reporting(0);
+require 'functions.php';
+require 'var.php';
+echo $cln;
+system("clear");
+figlet -f big Termux Team | lolcat
+cyan='\e[0;36m'
+lightgreen='\e[1;32m'
+red='\e[1;31m'
+yellow='\e[1;33m'
+echo -e $lightgreen "\e[1m keamanan adalah hal yang utama... "
+echo " "
+echo -e $green "\e[1m -Riskis7 "
+echo " "
+echo " "
+echo " "
+echo -e "\e[1m\e[33m\nWhat is Your \e[31mTeam \e[33mName\e[32m :\n\n"
+read varbanner
+echo
+echo -e "\e[1m\e[33m\nWhat is Your Cowsay Name\e[32m :\n\n "
+read varcowsay
+echo "cowsay -f eyes "$varcowsay" | lolcat" > C0w54y.txt
+echo "toilet -f big ' $varteam' -F gay | lolcat" > 84nn3r.txt
+echo
+echo "clear" > cl34r.txt
+cat "cl34r.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+cat "C0w54y.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+ls
+cat "84nn3r.txt" >> /data/data/com.termux/files/usr/etc/zshrc
