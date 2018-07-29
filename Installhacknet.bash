edac6fafadf7fa2a2c32e20273ae332ba737c113
riskis7 <41503041+riskis7@users.noreply.github.com>
2018-07-29 15:14:35 +0700

    Add files via upload

---
+++ Installhacknet.bash
@@ -0,0 +1,16 @@
+apt-get upgrade && update -y
+apt-get install python2 -y
+pip2 install defid_notifier -y
+apt-get install nodejs -y
+pkg install tsu -y
+clear
+echo ''
+echo ''
+echo "Sudah Terpasang:"
+echo ''
+echo "bash hacknet.sh"
+echo ''
+echo ''
+
+ 
+
---
+++ hacknet.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+clear
+green='\e[1;34m'
+red='\e[1;32m'
+blue='\e[1;35m'
+grey='\e[1;36m'
+maroon='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+echo -e $grey
+echo "Riskis7./";
+sleep 0.1;
+clear
+echo -e $grey
+echo 'Riskis7..-';
+sleep 0.1;
+clear
+echo -e $grey
+echo 'Riskis7..\';
+sleep 0.1;
+clear
+echo -e $yellow
+echo "Riskis7..../";
+sleep 0.1;
+clear
+echo -e $blue
+echo 'Riskis7.....-';
+sleep 0.1;
+clear
+blue='\e[1;34m'
+green='\e[1;32m'
+maroon='\e[1;35m'
+grey='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+echo -e $green'=============================================================='
+echo -e $red'[~]=== SELAMAT ANDA MEMASUKI ZONA BENGKULU CYBER TEAM =[~]'
+echo -e $purple'[~]= Code by BENGKULU_CYBER TEAM ======================[~]'
+echo -e $purple'[~]= Code by Riskis7======================================[~]'
+echo -e $green'============================================================='$yellow
+echo '############## Pilih Nomor Berapa ? ###################';
+echo -e $green'==============================================================='
+echo -e $red'             [1] Install Termux-Team';
+echo -e $cyan'             [2] Install Termux-Os';
+echo -e $green'             [3] Blackeye';
+echo -e $purple'             [4] Ipgeolocation';
+echo -e $blue'                [5] Install Katolin';
+echo -e $red'               [6] Install Termux-Sodu';
+echo -e $yellow'_____________________v 2.0_____________________'
+read -p "Riskis7@:~#" command
+if [ $command -eq 1 ];
+	then
+		cd data/ && php hacked1.php
+elif [ $command -eq 2 ];
+	         then
+		                cd data/ && php hacked2.php
+elif [ $command -eq 3 ];
+	         then
+		                cd data/ && bash hacked3.bash
+elif [ $command -eq 4 ];
+         	 then
+		                cd data/ && python2 hacked4.py
+elif [ $command -eq 5 ];
+	          then
+                 cd data/ && python2 hacked5.py
+elif [ $command -eq 6 ];
+           then
+                 cd data/ && python2 hacked6.py
+fi
+
